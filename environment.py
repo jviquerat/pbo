156796b641f1ea4b7e166bc26ed48fd6a4f97bb1
Jonathan Viquerat <jonathan.viquerat@mines-paristech.fr>
2020-04-20 09:18:31 +0200

    Initial commit

---
+++ environment.py
@@ -0,0 +1,186 @@
+# Generic imports
+import os
+import math
+import gym
+import numpy as     np
+from   gym   import spaces
+
+# Custom imports
+#from params  import *
+
+###############################################
+### Define gym environment
+###############################################
+
+###############################################
+### Class env_opt
+### A gym environment for optimization
+class env_opt(gym.Env):
+
+    ### Create object
+    def __init__(self, init_obs, n_params, x_min, x_max, y_min, y_max):#, rank, time):
+
+        # Init from parent class
+        super(env_opt, self).__init__()
+
+        # Fill structure
+        #self.rank           = rank
+        #self.path           = path+'_'+time+'/'
+        #self.output_path    = self.path+str(rank)+'/'
+
+
+        self.rank           = 0
+        time                = 'now'
+        path                = '.'
+        self.path           = path+'_'+time+'/'
+        self.output_path    = self.path+str(self.rank)+'/'
+        self.reward_path    = self.output_path#+reward_path
+        self.action_path    = self.output_path#+action_path
+        self.state_path     = self.output_path#+state_path
+        self.actions        = None
+        self.conv_actions   = None
+        self.last_actions   = None
+        self.states         = None
+        self.reward         = 0.0
+        self.net_rwd        = False
+        self.network        = None
+        self.n_params       = n_params
+        self.init_obs       = init_obs
+        self.x_min          = x_min
+        self.x_max          = x_max
+        self.y_min          = y_min
+        self.y_max          = y_max
+
+        # Define output reward folder
+        if (not os.path.exists(self.reward_path)):
+            os.makedirs(self.reward_path)
+
+        # Define output action folder
+        if (not os.path.exists(self.action_path)):
+            os.makedirs(self.action_path)
+
+        # Define output state folder
+        if (not os.path.exists(self.state_path)):
+            os.makedirs(self.state_path)
+
+        # Define action space
+        self.action_size  = self.n_params
+        self.action_space = spaces.Box(low   =-1.0,
+                                       high  = 1.0,
+                                       shape = (self.action_size,),
+                                       dtype = np.float16)
+
+        # Define observation space
+        self.state_size = self.n_params
+        self.observation_space = spaces.Box(low   =-1.0,
+                                            high  = 1.0,
+                                            shape = (self.state_size,),
+                                            dtype = np.float16)
+
+        # Set problem name
+        self.episode      =-1
+        self.totalstep    = 0
+
+    ### Provide next observation
+    def next_observation(self):
+
+        # Just an array of ones
+        obs = self.init_obs
+        #if (self.last_actions is not None):
+        #    obs = self.last_actions
+        #else:
+        #    obs = initial_value
+        #obs = np.random.rand(2)
+
+        # Fill states for file saving
+        self.states = obs
+
+        return obs
+
+    ### Choose next action
+    def take_action(self, actions):
+
+        # Reset action array
+        self.actions = []
+        self.conv_actions = self.action_size*[None]
+
+        # Convert actions
+        self.last_actions = actions
+        if (actions[0] < 0.0):
+            self.conv_actions[0] = abs(self.x_min)*actions[0]
+        else:
+            self.conv_actions[0] = self.x_max*actions[0]
+        if (actions[1] < 0.0):
+            self.conv_actions[1] = abs(self.y_min)*actions[1]
+        else:
+            self.conv_actions[1] = self.y_max*actions[1]
+        self.actions      = np.append(self.actions,
+                                      [actions, self.conv_actions])
+
+    ### Take one step
+    def step(self, actions):
+
+        # Take action and solve new problem
+        self.take_action(actions)
+
+        # Compute reward
+        self.reward = self.compute_reward(self.conv_actions)
+
+        # Collect observations
+        obs = self.next_observation()
+
+        # Update step index
+        self.totalstep += 1
+
+        # Episode end
+        done = True
+
+        return self.reward, self.conv_actions
+
+    ### Compute reward
+    def compute_reward(self, x):
+
+        # Set net_rwd to False by default
+        self.net_rwd = False
+
+        # If: - network must be used
+        #     - network is trained
+        #     - minimal generation is reached
+        #     - it is a network generation
+        # if (  use_network                 and
+        #       self.network.trained        and
+        #       self.totalstep >= net_start and
+        #     ((self.totalstep  - net_start)%net_freq == 0)):
+
+        #     # Default network reward value
+        #     self.net_rwd = penalty
+
+        #     # Safeguard
+        #     if (self.network.dataset_size == 0):
+        #         print('Error: the surrogate is not recognized')
+        #         exit()
+
+        #     # Predict reward
+        #     x            = np.reshape(x,(1,n_params))
+        #     self.net_rwd = True
+
+        #     return float(self.network.predict(x))
+
+        # Otherwise, compute reward normally
+        return -(x[0]**2 + x[1]**2)
+        #return -((1.0-x[0])**2 + 100.0*(x[1]-x[0]**2)**2)
+
+    ### Reset environment
+    def reset(self):
+
+        # Increment episode
+        self.episode += 1
+
+        # Provide initial observations
+        return self.next_observation()
+
+    ### Rendering
+    def render(self, mode='human', close=False):
+
+        # Empty for now
+        return 0
---
+++ plot_std.gnu
@@ -0,0 +1,22 @@
+reset
+set print "-"
+set term png truecolor size 900,600
+set output "parabola.png"
+set grid
+set style fill transparent solid 0.25 noborder
+set style line 1 lt 1 lw 3 pt 3 ps 0.5
+set style line 2 lt 2 lw 3 pt 3 ps 0.5
+set style line 3 lt 3 lw 3 pt 3 ps 0.5
+set style line 4 lt 4 lw 3 pt 3 ps 0.5
+
+# Compute ppo std bands
+set table 'ppo_lower.dat'
+plot 'ppo_avg_data.dat' u 1:3 smooth cspline w l
+set table 'ppo_upper.dat'
+plot 'ppo_avg_data.dat' u 1:4 smooth cspline w l
+unset table
+
+set logscale y
+
+plot "< paste ppo_lower.dat ppo_upper.dat"     u 1:2:5 w filledcurves lt 1 title "ppo dev", \
+     "ppo_avg_data.dat"   u 1:2 smooth csplines w l ls 1 title "ppo avg"
